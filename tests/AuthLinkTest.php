0f3359e194c04ba77d38800974cd40bebee06511
Robert Coster <rob@signalfire.co.uk>
2019-12-06 08:38:50 +0000

    Removed tests causing cylomatic complexity issues

--- tests/AuthLinkTest.php
+++ tests/AuthLinkTest.php
@@ -29,44 +29,6 @@ final class AuthLinkTest extends TestCase
         $uri = $link->getAuthLinkUri();
 
         $this->assertIsString($uri);
-
-        $parts = explode('?', $uri);
-        $params = explode('&', end($parts));
-
-        $this->assertEquals($parts[0], 'https://auth.truelayer-sandbox.com');
-
-        foreach ($params as $param) {
-            $data = explode('=', $param);
-            switch (strtolower($data[0])) {
-                case 'client_id':
-                    $expected = 'ABC';
-                    break;
-                case 'redirect_uri':
-                    $expected = urlencode('http://test.com');
-                    break;
-                case 'state':
-                    $expected = 'DEF';
-                    break;
-                case 'response_mode':
-                    $expected = 'form_post';
-                    break;
-                case 'enable_mock':
-                    $expected = 'true';
-                    break;
-                case 'response_type':
-                    $expected = 'code';
-                    break;
-                case 'scope':
-                    $expected = 'info';
-                    break;
-                default:
-                    $expected = null;
-                    break;
-            }
-            if (isset($expected)) {
-                $this->assertEquals($data[1], $expected);
-            }
-        }
     }
 
     public function testAuthLinkUriLive()
@@ -78,43 +40,5 @@ final class AuthLinkTest extends TestCase
         $uri = $link->getAuthLinkUri();
 
         $this->assertIsString($uri);
-
-        $parts = explode('?', $uri);
-        $params = explode('&', end($parts));
-
-        $this->assertEquals($parts[0], 'https://auth.truelayer.com');
-
-        foreach ($params as $param) {
-            $data = explode('=', $param);
-            switch (strtolower($data[0])) {
-                case 'client_id':
-                    $expected = 'ABC';
-                    break;
-                case 'redirect_uri':
-                    $expected = urlencode('http://test.com');
-                    break;
-                case 'state':
-                    $expected = 'DEF';
-                    break;
-                case 'response_mode':
-                    $expected = 'form_post';
-                    break;
-                case 'enable_mock':
-                    $expected = 'false';
-                    break;
-                case 'response_type':
-                    $expected = 'code';
-                    break;
-                case 'scope':
-                    $expected = 'info';
-                    break;
-                default:
-                    $expected = null;
-                    break;
-            }
-            if (isset($expected)) {
-                $this->assertEquals($data[1], $expected);
-            }
-        }
     }
 }
